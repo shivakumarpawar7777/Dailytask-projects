9a359a0747b6361e7b34f17cd9c0e494c62e1332
shivakumarpawar7777 <shivupawar90@gmail.com>
2024-11-22 11:55:39 +0530

    new file

---
+++ 15Nov Data type in Python.py
@@ -0,0 +1,117 @@
+#integer data type 
+age = 23  
+print(age)  
+print("The type of data",type(age))  
+
+shiv = 1234567890  
+print(shiv)  
+print("The type of data",type(shiv))  
+
+shiv = 987654321  
+print(shiv)  
+print("The type of data",type(shiv))  
+print("------------------------------------------------------------------------------") 
+
+#float datatype 
+float = 25.50  
+print(float)  
+print("The type of data",type(float))  
+
+a = 2.5  
+print(a)  
+print("The type of data",type(a))  
+
+etc = 9999.99999  
+print(etc)  
+print("The type of data",type(etc))  
+print("-------------------------------------------------------------------------------") 
+
+#complex datatype 
+a = 5.5+77j  
+print(a)  
+print("The type of data",type(a))  
+
+complex = 5+7j  
+print(complex)  
+print("The type of data",type(complex))  
+
+z = 9.7+77.5j  
+print(z)  
+print("The type of data",type(z))  
+print("-------------------------------------------------------------------------------------------") 
+
+#String:- 'single', "duble", """thriple""" 
+name = 'shiv'  
+print(name)  
+print("The type of data",type(name))  
+
+name = "shiv"  
+print(name)  
+print("The type of data",type(name))  
+
+name = """shivakumar   
+L  
+pawar"""  
+print(name)  
+print("The type of data",type(name))  
+print("--------------------------------------------------------------------------------------------") 
+
+#3.List:- A sequence elements in between a square bracket (1,2,3,4,5), mutable data type. 
+#Note: we can update the data. 
+z = [1,2,3,4,5,6,7,1,2,3,4] 
+print(z) 
+print("The type of data",type(z)) 
+
+numbers = [1,2,3,4,5,6,7,8,9,0] 
+print(numbers) 
+print("The type of data",type(numbers)) 
+
+list = [1,2,34,4,55,6,7,85,9,22] 
+print(list) 
+print("The type of data",type(list)) 
+
+#Tuple:- A sequence of characters in between a parathesis (1,2,3,4,5), immutable data type. 
+#Note: we con not update the data. 
+a = (20,-30.05,"""s""") 
+print(a) 
+print("The type of data",type(a)) 
+
+a = (10.105+10,0.2-0.4,2*2,10/2) 
+print(a) 
+print("The type of data",type(a))
+
+a = (20,-30.05,"shiv","2345",5+5) 
+print(a) 
+print("The type of data",type(a)) 
+
+#Set:- A sequence of characters in between a curly braces {1,2,3,4,5}, does not allow duplicate values. 
+a = {1,2,3,4,5,6} 
+print(a) 
+print("The type of data",type(a)) 
+
+a = {1+1,2-1,3*3} 
+print(a) 
+print("The type of data",type(a)) 
+
+a = {1+1,2-1,3*3,"shiv"} 
+print(a) 
+print("The type of data",type(a)) 
+
+#Dictionary:- A storing the elements in the form of key and values pair. 
+a = {"name":"shiv","age":25} 
+print(a) 
+print("The type of data",type(a)) 
+
+a = {"name":"book_p","price": 2*10} 
+print(a) 
+print("The type of data",type(a)) 
+
+a = {"pen":100,"price":1000*100} 
+print(a) 
+print("The type of data",type(a)) 
+
+#Boean:- True and Fales are two default values    
+
+#Print(type(True)) 
+#Print(type(Fales)) 
+#Print(True) 
