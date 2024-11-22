8c524d138f6403c282e20412aa6eae2bfbd7cfe8
shivakumarpawar7777 <shivupawar90@gmail.com>
2024-11-22 10:49:28 +0530

    changing the file name and added code

---
+++ 18Nov24-Operators in Python.py
@@ -0,0 +1,53 @@
+#Arithmetic operators
+"""a = 10
+b = 20
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+print(a // b)
+print("The type of data",type(a))
+print("The type of data",type(b))
+
+#Relation operators
+a = 50
+b = 20
+print(a == b)   # False
+print(a != b)   # True
+print(a < b)    # False      less then 
+print(a > b)    # True       greater then
+print(a <= b)    # False      less then 
+print(a >= b)    # True       greater then
+
+#logic operatores
+#and :- If the both value are true then the a and b value entire expression true.
+a = 50
+b = 20
+print("and operaters:", a == 50 and b == 20)
+print("and operaters:", a >= 50 and b != 20)
+print("and operaters:", a <= 50 and b >= 20)
+#not :- if the operand is false then the rntire not a expression true.
+a = 50
+b = 20
+print(not False)  # not false is True
+print(not True)   # not True is false 
+print("not operaters:", not a > b)
+print("not operaters:", not a < b)
+print("not operaters:", not a >= b)
+print("not operaters:", not a >= b)
+print("not operaters:", not a == b)
+
+# OR :- If either first or second opertoes is true, then it's true.
+a = 50
+b = 20
+print("OR operators:", (a > b) or (a == b))
+print("OR operators:", (a > b) or (a >= b))
+print("OR operators:", (a < b) or (a <= b))
+print("OR operators:", (a > b) or (a != b))"""
+
+name = input("Enter your name: ")
+print("welcome", name)
+age = input("age: ")
+print = ("your are entered", age)
