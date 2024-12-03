7a564af2ed847c6816300fd42b476a9227b92c9b
shivakumarpawar7777 <shivupawar90@gmail.com>
2024-12-03 13:49:24 +0530

    list

---
+++ list.py
@@ -0,0 +1,20 @@
+#list :- is buit in data type that can stores set of values like (int, float, string ..etc)
+# And its a mutenle data type.
+# []
+"""marks = [1, 2, 3, 4, 5, 6.5,"shiv"]
+print(type(marks))
+print(len(marks))
+print(marks[1])
+print(marks[-1])
+
+#list slicing 
+print(marks[1:7])
+print(marks[0:2])
+print(marks[1:-1])
+print(marks[2:5])
+print("-------------------------------")"""
+
+#list methods:-
+#append()
+list = [1,3,4,2,5,7,6]
+list.append(8)
