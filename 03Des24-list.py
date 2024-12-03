1c8936f132b8cb6fc8342f1a12262281d6c321e1
shivakumarpawar7777 <shivupawar90@gmail.com>
2024-12-03 18:50:44 +0530

    list add

--- 03Des24-list.py
+++ 03Des24-list.py
@@ -37,7 +37,7 @@ print("-------------------------------")
 
 list = [1,3,4,2,5,7,6]
 list.sort("shiv")             # we cannot use a string 
-print(list)"""
+print(list)
 
 #reverse() :- list.reverse()
 list = [1, 3, 4, 2, 5, 7, 6, 8]    #OUT PUT :- [8, 6, 7, 5, 2, 4, 3, 1]
@@ -57,4 +57,65 @@ list = "what you what od"    #assinment 1Q #OUT PUT :- do tahw uoy tahw
 print(list[::-1])
 
 list = "od tahw uoy tnaw"    #assinment 2Q #OUT PUT :- want you what do
-print(list[::-1])
+print(list[::-1])"""
+
+# aliasing & cloning
+data = "student data"
+data_copy = "studnt data"
+
+x = [1, 2, 3, 4, 5]
+y = x
+print(id(x))
+print(id(y))
+
+y[1] = 20
+print(id(x))
+print(id(y))
+print("----------------------------------")
+# cloning >>> slicing 
+a = [20, 30, 40, 50]
+b = x[:]
+print(id(a))
+print(id(b))
+
+b[1] = 200
+print(id(a))
+print(id(b))
+print("----------------------------------")
+
+# comporing of two list
+x = [1, 2, 3, 4, 5, 6]
+y = [1, 3, 5, 7 , 9, 6]
+
+print(x > y)
+print(y > x)
+print(x < y)
+print(y < x)
+
+
+x = ["ram", "shiv", "jay"]
+y = ["kiran", "sai", "ABC"]
+print(x > y)
+print(y > x)
+print(x < y)
+print(y < x)
+
+x = ["A", "B", "C"]
+y = ["a", "b", "c"]
+print(x > y)
+print(y > x)
+print(x < y)
+print(y < x)
+print("----------------------------------")
+
+#memberchip cheking list
+l = [1, 2, 3, 4, 5, 6]
+print(2 in l)
+print(10 not in l)
+
+# Nestet list
+l = [1,2,3,[4,5,6,[7,8,9]]]
+print(l[3])
+print(l[3][3])
+print(l[3][3][2])
+
