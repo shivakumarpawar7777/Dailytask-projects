232fd95dff9dd9980f6bece6bf56663484aac7ea
shivakumarpawar7777 <shivupawar90@gmail.com>
2024-12-17 20:02:10 +0530

    set

---
+++ 17des24.py
@@ -0,0 +1,34 @@
+#Write a program to create a set.
+set = {1, 2, 3, 4, 5}
+print(type(set))
+
+set = {}
+print(type(set))
+print(set)
+
+#Write program to iterate over sets.
+#Write a Python program to add member to a set.
+#Write a Python program to remove item from a given set.
+#Write a python program to create a intersection of set ?
+#Write a python program to createa unionof set ?
+#Write a python program to create set differance ?
+#Write a python program to create a symmetric defferance ?
+#write a python program to find max and min values in a set?
+#Given two Python sets, write a Python program to update the first set with items that exist only in the first set and not in the second set.
+#Write a Python program to remove items 10, 20, 30 from the following set at once.?
+#Check if two sets have any elements in common. If yes, display the common elements?
+#Update set1 by adding items from set2, except common items?
+#Remove items from set1 that are not common to both set1 and set2?
+
+#Write a python program to  add a key to a dictionary ?
+#Write a python program to check weather the given value is present in the dictionary or not ?
+#Write a Python script to print a dictionary where the keys are numbers between 1 and 15 (both included) and the values are the square of the keys.
+#Sample Dictionary
+#{1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144, 13: 169, 14: 196, 15: 225}
+#Write a python program to create a dictionary from the string ?
+#Write a python program to combine two dictionaries by adding values of common keys ?
+#Write a python program to merge two python dictionaries ?
+#Write a Python program to create a dictionary from a string.  Note: Track the count of the letters from the string.
+#Sample string : 'skywavessoftwares'
+#Expected output: {'w': 1, '3': 1, 'r': 2, 'e': 2, 's': 1, 'o': 1, 'u': 1, 'c': 1}
+
